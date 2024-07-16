125a2144773bd2fbc823537d7cbfb39c1c05a0a4
Amadeja <obsteter.amadeja@gmail.com>
2024-07-16 13:56:53 +0200

    Create not found view

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import { createWebHistory, createRouter } from "vue-router"
 import ToDoView from "./ToDoView.vue"
 import CalendarView from "./CalendarView.vue"
 import HomeView from "./HomeView.vue"
+import NotFoundView from "./NotFoundView.vue"
 
 const routes = [
     { path: "/", component: HomeView },
     { path: "/todo", component: ToDoView },
-    { path: "/calendar", component: CalendarView }
+    { path: "/calendar", component: CalendarView },
+    { path: '/:catchAll(.*)*', component: NotFoundView }
 ]
 
 const router = createRouter({
