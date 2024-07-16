0b2b186471ea5260e58e9a9b7c5465f0fe3d85ac
Amadeja <obsteter.amadeja@gmail.com>
2024-07-16 14:57:53 +0200

    Build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    base: process.env.NODE_ENV === 'production' ? '/vue-todo/' : '/',
+    base: '/vue-todo/',
   },
 })
